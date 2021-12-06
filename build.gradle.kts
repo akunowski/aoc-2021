0044a5d6812571967440b571802d4e53ea0cb570
Adrian Kunowski <adrian.kunowski@allegro.pl>
2021-12-06 14:49:17 +0100

    Project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,39 @@
 plugins {
     kotlin("jvm") version "1.6.0"
+    groovy
 }
+
 
 repositories {
     mavenCentral()
+}
+dependencies {
+    implementation("org.codehaus.groovy:groovy:3.0.8")
+    testImplementation("org.spockframework:spock-core:2.0-groovy-3.0")
+    testImplementation("org.codehaus.groovy:groovy-all:3.0.8")
+
 }
 
 tasks {
     sourceSets {
         main {
-            java.srcDirs("src")
+            kotlin {
+                java.srcDirs("src/main")
+            }
+        }
+
+        test {
+            groovy {
+                java.srcDirs("test/groovy")
+            }
         }
     }
 
     wrapper {
         gradleVersion = "7.3"
     }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
